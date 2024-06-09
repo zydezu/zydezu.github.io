6aa5d32fa90cbe1d763973824190a13ebe98377a
zydezu <kinomaxxing@gmail.com>
2024-06-09 11:13:10 +0100

    update

--- scripts/listaudiofiles.py
+++
@@ -1,8 +0,0 @@
-import os
-audiotypes = ["wav", "flac", "mp3", "ogg"]
- 
-for file in os.listdir():
-    if any(file.endswith(audiotype) for audiotype in audiotypes):
-        print(file)
-
-input()
